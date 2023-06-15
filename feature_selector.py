5a94dc146175da252754e8e83f3706486c02d727
Ali Reza Ghasemi <aghasemi@n17.cluster>
2023-06-15 15:59:48 -0400

    added all the features into feature_selector file

--- feature_selector.py
+++ feature_selector.py
@@ -50,6 +50,73 @@ class FeatureSelector():
             bootstraps.append(this_bootstrap)
 
         return bootstraps
+    
+    def chisquare(X, y, n_features):
+        '''
+        Runs chi-square feature selection on the data (X) and the target values (y) and finds
+        the index of the top n_features number of features.
+    
+        Args:
+            X: A Numpy array containing the dataset
+            y: A Numpy array consisting of the target values
+            n_features: An integer specifying how many features should be selected
+    
+        Returns a list containing the indices of the features that have been selected
+        '''
+        chi2_score, p_val = chi2(X, y)
+        index = list(np.argsort(list(chi2_score))[-1*n_features:])
+        index.sort()
+        return index
+    
+    def infogain(X, y, n_features):
+        '''
+        Runs infogain feature selection on the data (X) and the target values (y) and finds
+        the index of the top n_features number of features.
+    
+        Args:
+            X: A Numpy array containing the dataset
+            y: A Numpy array consisting of the target values
+            n_features: An integer specifying how many features should be selected
+    
+        Returns a list containing the indices of the features that have been selected
+        '''
+        score = mutual_info_classif(X, y,random_state=0)
+        index = list(np.argsort(list(score))[-1*n_features:])
+        index.sort()
+        return index
+    
+    def mrmr(X, y, **kwargs):
+        """
+        This function implements the MRMR feature selection
+        Input
+        -----
+        X: {numpy array}, shape (n_samples, n_features)
+            input data, guaranteed to be discrete
+        y: {numpy array}, shape (n_samples,)
+            input class labels
+        kwargs: {dictionary}
+            n_selected_features: {int}
+                number of features to select
+        Output
+        ------
+        F: {numpy array}, shape (n_features,)
+            index of selected features, F[0] is the most important feature
+        J_CMI: {numpy array}, shape: (n_features,)
+            corresponding objective function value of selected features
+        MIfy: {numpy array}, shape: (n_features,)
+            corresponding mutual information between selected features and response
+        Reference
+        ---------
+        Brown, Gavin et al. "Conditional Likelihood Maximisation: A Unifying Framework for Information Theoretic Feature Selection." JMLR 2012.
+        """
+        if 'n_selected_features' in kwargs.keys():
+            n_selected_features = kwargs['n_selected_features']
+            F, J_CMI, MIfy= LCSI.lcsi(X, y, gamma=0, function_name='MRMR', n_selected_features=n_selected_features)
+        else:
+            
+            F, J_CMI, MIfy = LCSI.lcsi(X, y, gamma=0, function_name='MRMR')
+        return F
+
 
 '''
 Below is an example usage
