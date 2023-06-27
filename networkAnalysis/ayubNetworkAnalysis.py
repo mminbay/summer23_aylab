264111016f985bca43dc4289ccb3c1e498931a2a
Ahmed Ayub Khan <akhan@colgate.edu>
2023-06-26 21:12:22 -0400

    made network analysis more readable, added utility functions etc.

--- networkAnalysis/ayubNetworkAnalysis.py
+++ networkAnalysis/ayubNetworkAnalysis.py
@@ -1,57 +1,118 @@
-# Make sure you run this code cell everytime you open the notebook!
-import networkx as nx            # Import NetworkX library as nx for its network functions
+import networkx as nx
 from networkx.algorithms.community import k_clique_communities
-import numpy as np               # Import NumPy library as np for its mathematical functions
-import matplotlib.pyplot as plt  # Import Matplotlib library as plt for its plotting functions
-import matplotlib.patches as mpatches
-import math
-# import plotly.express as px
 import pandas as pd
+import matplotlib.pyplot as plt
 
+############# Functions for Plotting and Network Analyses #############
 
-
-############# functions for plotting and network analyses #############
-
-# A function defined for simple network plotting
+# Function to plot the graph
 def plot_graph(G):
+    """
+    Plot the graph using NetworkX library.
+
+    Args:
+        G (nx.Graph): The input graph.
+    """
     pos = nx.drawing.layout.circular_layout(G)
-    plt.figure(figsize=(7,6))                                                             # Specify the figure size
+    plt.figure(figsize=(7, 6))    # Specify the figure size
     # in the next line, nx.draw(...) had an error, changing it to nx.draw_networkx(...) fixes it
-    nx.draw_networkx(G, pos=pos, with_labels=True, font_weight='bold', node_size=500, \
-    node_color='red', font_size=16, font_color='white',width=3, edge_color='green') # The figure properties
-    plt.show()                                                                            # Show the network
-    # uncomment the following line of code to save the simple network plot to a [.png]. change the path to wherever you want to output it.
+    nx.draw_networkx(
+        G,
+        pos=pos,
+        with_labels=True,
+        font_weight='bold',
+        node_size=500,
+        node_color='red',
+        font_size=16,
+        font_color='white',
+        width=3,
+        edge_color='green'
+    ) # can change the figure properties from among the above as suitable
+    
+    plt.show()
+    # Uncomment the following line to save the network plot as a PNG file
     # if you want to output it in the same directory, write "./network.png" instead of what I have
-    plt.savefig("../data/networkAnalysisData/network.png", format="PNG")
+    # plt.savefig("../data/networkAnalysisData/network.png", format="PNG")
 
 
-# A function that provides some network summary information.
+# Function to get summary information about the graph
 def get_graph_info(graph):
+    """
+    Print summary information about the graph.
+
+    Args:
+        graph (nx.Graph): The input graph.
+    """
     print("Number of nodes:", graph.number_of_nodes())
     print("Number of edges:", graph.number_of_edges())
     if type(graph) == nx.classes.digraph.DiGraph:
-        print("Number of weakly connected components:",
-              len(list(nx.weakly_connected_components(graph))))
+        print("Number of weakly connected components:", len(list(nx.weakly_connected_components(graph))))
     else:
         print("Number of connected components:", len(list(nx.connected_components(graph))))
     print("Node degree:", dict(graph.degree()))
+    print('\n')
+
+    
+# Function to find the nodes with the highest centrality score
+def find_highest_centrality_nodes(centralities, centrality_metric, is_node_metric):
+    """
+    Find the nodes with the highest centrality score.
+
+    Args:
+        centralities (dict): Dictionary of centrality scores for nodes.
+        centrality_metric (str): The centrality metric used.
+
+    """
+    itemMaxValue = max(centralities.items(), key=lambda x: x[1])
+    
+    listOfKeys = list()
+    for key, value in centralities.items():
+        if value == itemMaxValue[1]:
+            listOfKeys.append(key)
+    if is_node_metric:
+        print('Node(s) with the maximum', centrality_metric, 'centrality score:', listOfKeys)
+        print('The maximum', centrality_metric, 'centrality score in the network: ', itemMaxValue[1])
+    else:
+        print('Edge(s) with the maximum', centrality_metric, 'centrality score:', listOfKeys)
+        print('The maximum edge', centrality_metric, 'centrality score in the network: ', itemMaxValue[1])
+    print('\n')
 
 
-# A function to print the highest centrality, and the corresponding nodes
-def find_highest_centrality_nodes(centralities, centrality_metric):
-  itemMaxValue = max(centralities.items(), key=lambda x: x[1])
-  print('The maximum', centrality_metric, 'centrality score in the network : ', itemMaxValue[1])
-  listOfKeys = list()
-  # Iterate over all the items in dictionary to find keys with max value
-  for key, value in centralities.items():
-      if value == itemMaxValue[1]:
-          listOfKeys.append(key)
-  print('Nodes with the maximum', centrality_metric, 'centrality score: ', listOfKeys)
+# function to print centrality info
+def print_centrality_info(centralities, centrality_metric, highest_only=False, is_node_metric = True):
+    """
+    Print centrality information for nodes.
 
+    Args:
+        centralities (dict): Dictionary of centrality scores for nodes.
+        centrality_metric (str): The centrality metric used.
+        highest_only (bool): Whether to print only the highest centrality values(node/edge) or all centrality values.
+    """
+    if not highest_only:
+        print("The", centrality_metric, "centrality for all nodes:", centralities)
+    find_highest_centrality_nodes(centralities, centrality_metric, is_node_metric)
 
+    
+# Function to load data from CSV files
+def load_data(edges_path, nodes_path):
+    """
+    Load data from edges and nodes CSV files.
 
+    Args:
+        edges_path (str): Path to the edges CSV file.
+        nodes_path (str): Path to the nodes CSV file.
 
-#loading data:
+    Returns:
+        pd.DataFrame: The loaded edges data.
+        pd.DataFrame: The loaded nodes data.
+    """
+    edges = pd.read_csv(edges_path, skiprows=1, sep=',', names=['SNP1', 'SNP2'])
+    nodes = pd.read_csv(nodes_path, skiprows=1, sep=',', names=['SNPid'])
+    return edges, nodes
+
+    
+############# Main Code #############
+
 '''
     the code snippet below loads the data from 2 csv files, nodes and edges. I put both files in the same directory. 
     the edges file should have 2 SNP columns (at least for me) 
@@ -72,102 +133,67 @@ def find_highest_centrality_nodes(centralities, centrality_metric):
     where col1 is an ID column, not necessary
 '''
 
-#### loading a data from edges and nodes csv files. file is described above. ####
-#### follow the line-by-line instructions if present ####
+# Define the paths to the edges and nodes CSV files
+edges_path = '../data/networkAnalysisData/randomNetworkAnalysisData_edges.csv'
+nodes_path = '../data/networkAnalysisData/randomNetworkAnalysisData_nodes.csv'
 
-#change the paths to match your edges and nodes file paths, and change the column names to match your file's column names
-edges = pd.read_csv('../data/networkAnalysisData/randomNetworkAnalysisData_edges.csv', skiprows = 1, sep=',', names=['SNP1', 'SNP2'])
-nodes = pd.read_csv('../data/networkAnalysisData/randomNetworkAnalysisData_nodes.csv', skiprows = 1, sep =',', names = ['SNPid'])
+# Load data from edges and nodes CSV files
+edges, nodes = load_data(edges_path, nodes_path)
 
-
-
-
-#### plotting a graph using the data above, and using the plotting functions defined at the top ####
-#### follow the line-by-line instructions if present ####
+# Create the graph
 G = nx.from_pandas_edgelist(edges, 'SNP1', 'SNP2')
+
+# Print graph information
 get_graph_info(G)
 
-# print(edges.head()) # .head() shows the first few rows of your dataframe
-# print(nodes.head())
+# Plot the graph
+plot_graph(G) # download and use gephi software to make a better (s*xier) looking graph 
 
-plot_graph(G) # download and use gephi software to make a s*xier graph (maybe watch a youtube tutorial if unfamiliar)
-
-
-
-
-
-
-
-
-#### Network Analyses Unweighted ####
-# some of these may use the functions from the top of this file
+############# Network Analyses Unweighted #############
 
 # A. CENTRALITIES
 
-# 1. DEGREE CENTRALITY, i.e. the number of links/edges to other nodes that a certain node has is its degree centrality
 
-centralities=nx.degree_centrality(G)                               # Find the degree centrality of all nodes
-# print("The degree centrality for all nodes: ", centralities)       # uncomment to see: All degree centrality of the nodes in the network G.
-find_highest_centrality_nodes(centralities,'degree')               # The nodes with highest degree centrality
-
+# 1. Degree Centrality
+''' i.e. the number of links/edges to other nodes that a certain node has is its degree centrality '''
+centralities = nx.degree_centrality(G)                               # degree centrality for all nodes             
+print_centrality_info(centralities, 'degree', highest_only=False)      # prints node(s) with the highest degree centrality values, as well as other info if highest_only=false
+# each centrality measure follows similar 2 lines of code so they don't have comments explaining them
 # following finds the degree centrality of node x. replace x with an index that is not out of bounds, then uncomment:
-# print("The degree centrality for node x is ", centralities[x - 1])  # -1 cuz 
-# can use this print command to show any of the other centralities for a specific node too, just copy/paste it under any other centrality metric below
+# print("The degree centrality for node 1 is ", centralities[1], '\n')  
 
 
-# 2. HARMONIC CENTRALITY is a centrality measure related to closeness centrality, but better since it works for disconnected networks. 
+# 2. Harmonic Centrality
+''' this is a centrality measure related to closeness centrality, but better since it works for disconnected networks too '''
+centralities = nx.harmonic_centrality(G)
+print_centrality_info(centralities, 'harmonic', highest_only=True)
 
-# closeness centrality is how close a node is to all other nodes. it is measured using (reciprocal of ) the sum of shortest paths that all other nodes have to this node.
-# thus if the shortest paths' sum is small, its reciprocal (and closeness centrality) is large
+# 3. Betweenness Centrality
+''' depends on how many shortest paths a node lies on (or shortest paths an edge exists in), higher the better '''
+# a. node-betweenness
+centralities = nx.betweenness_centrality(G)
+print_centrality_info(centralities, 'betweenness', highest_only=True)
+# b. edge-betweenness
+centralities=nx.edge_betweenness_centrality(G)                             
+print_centrality_info(centralities, 'betweenness', highest_only=True, is_node_metric=False)                 
 
-centralities=nx.harmonic_centrality(G)                               # Find the harmonic centrality of all nodes
-# print("The harmonic centrality for all nodes: ", centralities)       # All harmonic centrality of the nodes in the network G.
-find_highest_centrality_nodes(centralities,'harmonic')               # The nodes with highest harmonic centrality
-
-
-#3. BETWEENNESS CENTRALITY: depends on how many shortest paths a node lies on (or shortest paths an edge exists in), higher the better
-
-#a. node betweenness
-centralities=nx.betweenness_centrality(G)                            # Find betweenness centrality of all nodes
-# print("The betweenness centrality for all nodes: ", centralities)    # All betweenness centrality of the nodes in the network G.
-find_highest_centrality_nodes(centralities,'betweenness')            # The nodes with highest betweenness centrality
-#b. edge betweenness
-centralities=nx.edge_betweenness_centrality(G)                              # Edge betweenness centrality for all nodes
-# print("The betweenness centrality for all edges: ", centralities)
-find_highest_centrality_nodes(centralities, 'betweenness')                  # The highest edge betweenness centrality and the corresponding edges
-
-
-#4. PAGE RANK. PageRank computes a ranking of the nodes in the graph G based on the structure of the incoming links. 
-# It was originally designed as an algorithm to rank web pages.
-centralities=nx.pagerank(G)                                          # Find the pagerank centrality of all nodes
-# print("The pagerank centrality for all nodes: ", centralities)       # All pagerank centrality of the nodes in the network G.
-find_highest_centrality_nodes(centralities,'pagerank')               # The nodes with highest pagerank centrality
-
+# 4. PageRank Centrality
+''' PageRank computes a ranking of the nodes in the graph G based on the structure of the incoming links. 
+    It was originally designed as an algorithm to rank web pages. '''
+centralities = nx.pagerank(G)
+print_centrality_info(centralities, 'PageRank', highest_only=True)  
 
 
 # B. COMMUNITIES
+'''
+ k-clique communities, or CFINDER: finds k-clique overlapping communities using clique percolation. google clique percolation and cfinder for details, but:
+ in summary what it does is it divides the nodes up in communities, and allows one node to be a member of multiple communities
+ which previous algorithms, like Girvan Newman algorithm, could not do
+'''
 
-#6. CFINDER: finds k-clique overlapping communities using clique percolation. google clique percolation and cfinder for details, but:
-# basically what it does is it divides the nodes up in communities, and allows one node to be a member of multiple communities
-# which previous algorithms, like Girvan Newman algorithm, could not do
+# 5. k-Clique Communities
 
-# Find 3-clique communities using k-clique communities
-cliques = nx.algorithms.community.k_clique_communities(G, 3)
-
-# Print the 3-clique communities
+cliques = k_clique_communities(G, 3) # finds 3-clique communities, change the 3 to find bigger-clique communities
 for i, clique in enumerate(cliques, start=1):
     print(f"3-Clique Community {i}: {clique}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print('\n')
