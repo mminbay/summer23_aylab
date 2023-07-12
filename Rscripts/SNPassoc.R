5a2ec10ff37e7054baa95b1cbca799bb4c1c97e0
mminbay <mminbay@colgate.edu>
2023-07-12 17:11:05 -0400

    Update SNPassoc.R

--- Rscripts/SNPassoc.R
+++ Rscripts/SNPassoc.R
@@ -1,8 +1,9 @@
-#uncomment this if you need to install packages
-# install.packages("SNPassoc") # nolint
-# install.packages("dplyr") # nolint
+# uncomment this if you need to install packages
+# install.packages("SNPassoc", repos = "http://cran.us.r-project.org") # nolint
+# install.packages("dplyr",  repos = "http://cran.us.r-project.org") # nolint
+
 library("SNPassoc")
-library(dplyr)
+library("dplyr")
 
 
 ###########################################
@@ -12,7 +13,7 @@ library(dplyr)
 # reading the data file with all the important SNPs, as well as the covariating clinical factors of importance # nolint
 
 # Read the CSV file, replace path with your file path
-snpdata <- read.csv("SNP_assoc_testing.csv")
+snpdata <- read.csv("data/analysis_data.csv")
 
 # Select column names starting with "rs". this selects all the SNP columns that start with rs # nolint
 
@@ -24,8 +25,10 @@ snp_column_names <- names(snpdata)[grepl("^rs", names(snpdata))]
 # Get column numbers corresponding to SNP names
 snp_column_numbers <- which(names(snpdata) %in% snp_column_names)
 
+start <- min(snp_column_numbers)
+end <- max(snp_column_numbers)
 # Create a range of column numbers in the form [1:10]. make sure they are consecutive # nolint
-column_range <- paste0("[", min(snp_column_numbers), ":", max(snp_column_numbers), "]") # nolint
+column_range <- paste0("[", start, ":", end, "]") # nolint
 
 # Print the selected column names and column range. this shows you where the snps are in your file # nolint
 print(snp_column_names)
@@ -37,10 +40,10 @@ print(column_range)
 ###############
 
 # change clinical factors and target as you want
-clinical <- c("Sex", "Age", "Chronotype", "Sleeplessness.Insomnia")
+clinical <- c("Sex", "Age", "Chronotype", "Sleeplessness_Insomnia", "TSDI")
 target <- "PHQ9_binary"
 
-snpcols <- colnames(snpdata)[1:10]
+snpcols <- colnames(snpdata)[min(snp_column_numbers):max(snp_column_numbers)]
 
 # following snippet changes 0 to AA, 1 to AB
 # since we have already binarized our data according to dominant model
@@ -48,7 +51,7 @@ snpcols <- colnames(snpdata)[1:10]
 # since dominant model compares AA vs (AB and BB) we can choose to represent
 # 1 as either AB or BB as both of them are converted to 1 in dominant model
 snpdata <- snpdata %>%
-  mutate_at(vars(1:10),
+  mutate_at(vars(start:end),
             list(~ ifelse(. == 0, "AA", ifelse(. == 1, "AB", .))))
 
 
@@ -64,8 +67,8 @@ result.snp = interactionPval(as.formula(paste("PHQ9_binary~",
                          data.snp, model = "do")
 
 # change the file path to save the result files there
-write.csv(result.snp, file = "./data/snpAssocResults/snp-snp_interaction.csv", row.names = TRUE) # nolint
-png("./data/snpAssocResults/snp-snp_interaction.png", width = 8, height = 6, units = "in", res = 300)  # Adjust the resolution as needed # nolint
+write.csv(result.snp, file = "data/snp-snp_interaction.csv", row.names = TRUE) # nolint
+png("data/snp-snp_interaction.png", width = 8, height = 6, units = "in", res = 300)  # Adjust the resolution as needed # nolint
 par(mar = c(4, 4, 2, 1))  # Adjust the margin values as needed
 plot(result.snp)
 dev.off()
